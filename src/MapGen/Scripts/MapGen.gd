7da70deee0eaed0ae88c646ee9e63097488815a3
Yni-Viar <81305791+Yni-Viar@users.noreply.github.com>
2024-07-13 17:33:41 +0300

    Add zone support
    Currently, without checkpoints

--- src/MapGen/Scripts/MapGen.gd
+++ src/MapGen/Scripts/MapGen.gd
@@ -6,7 +6,7 @@ var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 enum RoomTypes {ROOM1, ROOM2, ROOM2C, ROOM3, ROOM4, EMPTY}
 
 ## Rooms that will be used
-@export var rooms: MapGenRoom
+@export var rooms: Array[MapGenRoom]
 #currentluy not ported to gdscript
 #@export var generate_more_endrooms: bool = false
 #@export var generate_more_crossrooms: bool = false
@@ -14,6 +14,12 @@ enum RoomTypes {ROOM1, ROOM2, ROOM2C, ROOM3, ROOM4, EMPTY}
 @export var size: int = 12
 ## Room in grid size
 @export var grid_size: float = 20.48
+## Divide the map on zones
+@export var enable_zones: bool = false
+## Zone transitions (first transition MUST be 0, last - more then size value, and the rest divide the zones)
+@export var zone_transitions: Array[int] = []
+# currently buggy
+# @export var checkpoints: Array[PackedScene] = []
 var endroom_amount: int
 var hallway_amount: int
 var corner_amount: int
@@ -23,6 +29,15 @@ var crossroom_amount: int
 class TempRoom:
 	var type: RoomTypes
 	var angle: float
+
+func get_zone(y)->int:
+	var zone = 0
+	if enable_zones:
+		for i in range(zone_transitions.size() - 1):
+			if y >= zone_transitions[i] && y < zone_transitions[i + 1]:
+				zone = i
+				break
+	return zone
 
 func create_map():
 	var x: int
@@ -69,9 +84,16 @@ func create_map():
 		if y - height < 1:
 			height = y
 		# height for each zone
-		#var yhallways: int = rng.randi_range(0, 1) + int(size / 6)
+		var yhallways: int
+		if !enable_zones:
+			yhallways == height
+		else:
+			yhallways = rng.randi_range(0, 1) + int(size / 5)
+			if get_zone(y - height) != get_zone(y - height - 1):
+				height -= 1
+		
 		# this FOR loop cleans the mapgen from loops.
-		for j in range(1, height):
+		for j in range(1, yhallways):
 			x2 = max(min(rng.randi_range(0, width - 2) + x, int(size * 0.8)), 2)
 			while temp_room[x2][y - 1].angle >= 0 || temp_room[x2 - 1][y - 1].angle >= 0 || temp_room[x2 + 1][y - 1].angle >= 0:
 				x2 += 1
@@ -88,6 +110,9 @@ func create_map():
 					tmpheight = (rng.randi_range(0, height - 1)) + 1;
 				
 				for y2 in range(y - tmpheight, y + 1):
+					#if enable_zones && get_zone(y2) != get_zone(y2 + 1):
+						#temp_room[x2][y2].angle = 180
+					#else:
 					temp_room[x2][y2].angle = 0;
 
 				if (tmpheight == height):
@@ -111,6 +136,8 @@ func create_map():
 			var east: bool
 			var south: bool
 			var west: bool
+			#if temp_room[l][m].angle == 180:
+				#continue
 			if temp_room[l][m].angle == 0:
 				if l > 0:
 					west = (temp_room[l - 1][m].angle > -1)
@@ -206,62 +233,73 @@ func create_map():
 				temp_room[l][m].type = RoomTypes.EMPTY
 	
 	var selected_room: PackedScene
-	var room1_count: int = 0
-	var room2_count: int = 0
-	var room2c_count: int = 0
-	var room3_count: int = 0
-	var room4_count: int = 0
+	var room1_count: Array[int] = [0]
+	var room2_count: Array[int] = [0]
+	var room2c_count: Array[int] = [0]
+	var room3_count: Array[int] = [0]
+	var room4_count: Array[int] = [0]
+	
+	if enable_zones:
+		for i in range(zone_transitions.size()):
+			room1_count.append(0)
+			room2_count.append(0)
+			room2c_count.append(0)
+			room3_count.append(0)
+			room4_count.append(0)
 	#spawn a room
 	for n in range(size):
 		for o in range(size):
 			var room: StaticBody3D
 			match temp_room[n][o].type:
 				RoomTypes.ROOM1:
-					if (room1_count >= rooms.endrooms_single.size()):
-						selected_room = rooms.endrooms[rng.randi_range(0, rooms.endrooms.size() - 1)]
+					if (room1_count[get_zone(o)] >= rooms[get_zone(o)].endrooms_single.size()):
+						selected_room = rooms[get_zone(o)].endrooms[rng.randi_range(0, rooms[get_zone(o)].endrooms.size() - 1)]
 					else:
-						selected_room = rooms.endrooms_single[room1_count]
-					room1_count += 1
+						selected_room = rooms[get_zone(o)].endrooms_single[room1_count[get_zone(o)]]
+					room1_count[get_zone(o)] += 1
 					room = selected_room.instantiate()
 					room.position = Vector3(n * grid_size, 0, o * grid_size)
 					room.rotation_degrees = Vector3(0, temp_room[n][o].angle, 0)
 					add_child(room, true)
 				RoomTypes.ROOM2:
-					if (room2_count >= rooms.hallways_single.size()):
-						selected_room = rooms.hallways[rng.randi_range(0, rooms.hallways.size() - 1)]
+					#if enable_zones && get_zone(o) != get_zone(o + 1):
+						#selected_room = checkpoints[get_zone(o) - 1]
+					#else:
+					if (room2_count[get_zone(o)] >= rooms[get_zone(o)].hallways_single.size()):
+						selected_room = rooms[get_zone(o)].hallways[rng.randi_range(0, rooms[get_zone(o)].hallways.size() - 1)]
 					else:
-						selected_room = rooms.hallways_single[room2_count]
-					room2_count += 1
+						selected_room = rooms[get_zone(o)].hallways_single[room2_count[get_zone(o)]]
+					room2_count[get_zone(o)] += 1
 					room = selected_room.instantiate()
 					room.position = Vector3(n * grid_size, 0, o * grid_size)
 					room.rotation_degrees = Vector3(0, temp_room[n][o].angle, 0)
 					add_child(room, true)
 				RoomTypes.ROOM2C:
-					if (room2c_count >= rooms.corners_single.size()):
-						selected_room = rooms.corners[rng.randi_range(0, rooms.corners.size() - 1)]
+					if (room2c_count[get_zone(o)] >= rooms[get_zone(o)].corners_single.size()):
+						selected_room = rooms[get_zone(o)].corners[rng.randi_range(0, rooms[get_zone(o)].corners.size() - 1)]
 					else:
-						selected_room = rooms.corners_single[room2c_count]
-					room2c_count += 1
+						selected_room = rooms[get_zone(o)].corners_single[room2c_count[get_zone(o)]]
+					room2c_count[get_zone(o)] += 1
 					room = selected_room.instantiate()
 					room.position = Vector3(n * grid_size, 0, o * grid_size)
 					room.rotation_degrees = Vector3(0, temp_room[n][o].angle, 0)
 					add_child(room, true)
 				RoomTypes.ROOM3:
-					if (room3_count >= rooms.trooms_single.size()):
-						selected_room = rooms.trooms[rng.randi_range(0, rooms.trooms.size() - 1)]
+					if (room3_count[get_zone(o)] >= rooms[get_zone(o)].trooms_single.size()):
+						selected_room = rooms[get_zone(o)].trooms[rng.randi_range(0, rooms[get_zone(o)].trooms.size() - 1)]
 					else:
-						selected_room = rooms.trooms_single[room3_count]
-					room3_count += 1
+						selected_room = rooms[get_zone(o)].trooms_single[room3_count[get_zone(o)]]
+					room3_count[get_zone(o)] += 1
 					room = selected_room.instantiate()
 					room.position = Vector3(n * grid_size, 0, o * grid_size)
 					room.rotation_degrees = Vector3(0, temp_room[n][o].angle, 0)
 					add_child(room, true)
 				RoomTypes.ROOM4:
-					if (room4_count >= rooms.crossrooms_single.size()):
-						selected_room = rooms.crossrooms[rng.randi_range(0, rooms.crossrooms.size() - 1)]
+					if (room4_count[get_zone(o)] >= rooms[get_zone(o)].crossrooms_single.size()):
+						selected_room = rooms[get_zone(o)].crossrooms[rng.randi_range(0, rooms[get_zone(o)].crossrooms.size() - 1)]
 					else:
-						selected_room = rooms.crossrooms_single[room4_count]
-					room4_count += 1
+						selected_room = rooms[get_zone(o)].crossrooms_single[room4_count[get_zone(o)]]
+					room4_count[get_zone(o)] += 1
 					room = selected_room.instantiate()
 					room.position = Vector3(n * grid_size, 0, o * grid_size)
 					room.rotation_degrees = Vector3(0, temp_room[n][o].angle, 0)
